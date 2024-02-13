653b4631af650a1e162eea0519c08732763b9aeb
zeyaurrahman786 <zeyaurrahman078608@gmail.com>
2024-02-14 03:20:01 +0530

    Music Player

--- Level 2/Project-3-Music Player/script.js
+++ Level 2/Project-3-Music Player/script.js
@@ -19,19 +19,28 @@ array.forEach(function(element,index){
 var array = [
     {songName:"Jale2",url:"./songs/Jale 2.mp3",image:"./posters/jale.jpg"},
     {songName:"Arjan Vailly",url:"./songs/Arjan Vailly Ne.mp3",image:"./posters/animal.jpg"},
-    {songName:"Nathuniya Par 9 Go",url:"./songs/Nathuniya Par 9 Go.mp3",image:"./posters/Pawan Singh.jpeg"},
-    {songName:"Nimbu Kharbuja Bahil Madam",url:"./songs/Nimbu Kharbuja Bhail Madam.mp3",image:"./posters/Khesari Lal.jpeg"},
-]
+    {songName:"Nathuniya Par 9 Go",url:"./songs/Nathuniya Par 9 Go.mp3",image:"./posters/PawanSingh.jpg"},
+    {songName:"Nimbu Kharbuja Bahil Madam",url:"./songs/Nimbu Kharbuja Bhail Madam.mp3",image:"./posters/KhesariLal.jpg"}
+];
+
+var allSongs = document.querySelector("#all-songs")
+
+var audio = new Audio();
 
 var clutter = "";
-array.forEach(function(){
-    clutter += `{<div class="song-card">
+
+array.forEach(function(element,index){
+    clutter += `<div class="song-card" id=${index}>
     <div class="part1">
-        <img src="https://wallpaperbuzz.net/wp-content/uploads/2023/11/Animal-HD-WAllpaper-9-1536x1351.jpg" alt="">
-    <h2>Pahle Bhi Main</h2>
+        <img src=${element.image} alt="">
+    <h2>${element.songName}</h2>
     </div>
     <h6>3:56</h6>
-</div>}`;
+</div>`;
 })
 
-document.querySelector("#all-songs").innerHTML = clutter;
+allSongs.innerHTML = clutter;
+
+allSongs.addEventListener("click",function(details){
+    console.log(array[details.target.id]);
+})
